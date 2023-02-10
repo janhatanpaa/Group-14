224bdbe578d32e6fa52f763db99e2a32ea1d624d
Joonas Sillanaukee <joonas.sillanauke3@gmail.com>
2023-02-10 10:56:50 +0200

    työhakulomake

--- php/tyonhakulomake.php
+++ php/tyonhakulomake.php
@@ -4,12 +4,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home - Rastorinte</title>
+    <link rel="icon" type="image/x-icon" href="assets/rastorintefavicon.png">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <meta name="description" content="Tasty meals with excellent service">
+    <meta name="keywords" content="Rastorinte, Restaurant">
+    <meta name="author" content="Jan, Jami, Joonas, Otto">
 </head>
 <body>
-
-    <form action='tehtava3_arvot.php' method='post'>
-    <input type='submit' value='Ok'>
+<header>
+    <a href="index.html"><img class="logo" src="assets/rastorinte.png" alt="restaurant-rastorinte"></a>
+</header>
+<nav>
+    <div class="navbar grey topBotomBordersIn">
+        <a href="menu.html" style="font-size: 15px;">Menu</a>
+        <a href="index.html" style="font-size: 24px;">Home</a>
+        <a href="gallery.html" style="font-size: 15px;">Gallery</a>
+    </div>
+</nav>
+<main> 
+    <h1>Työnhakulomake</h1>
+   
+</main>
+<section>
+    <div class="laatikko">
+        <form action='tallenna.php' method='post'> 
+            Etunimi:<input type='text' name='etunimi' value=''><br>
+            <br>
+            Sukunimi:<input type='text' name='sukunimi' value=''><br>
+            <br>
+            Ikä:<input type='text' name='ika' value=''><br>
+            <br>
+            <input type='submit' name='ok' value='OK'><br> 
+    </div>
     
+</section>
+
+    
+    
+<footer>
+<div class="footer">  
+    <div class="row">
+        <div class="column use-links">
+            <p>Rastorinte</p>
+            
+                <p><a href="menu.html" style="font-size: 13px;">Menu</a></p>
+                <p><a href="index.html" style="font-size: 13px;">Home</a></p>
+                <p><a href="gallery.html" style="font-size: 13px;">Gallery</a></p>
+            </div>
+        <div class="column use-links">
+            <p>Follow Us</p>          
+                <p><a href="https://www.facebook.com/" style="font-size: 13px;">Facebook</a></p>
+                <p><a href="https://www.instagram.com/" style="font-size: 13px;">instagram</a></p>
+            </div>       
+        <div class="column use-links">
+            <p>Address</p>
+                <p style="font-size: 13px;">Jokutie 2 13100 Hämeenlinna</p>
+                <p><a id="phone" style="font-size: 13px;">+35840 5656767 </a></p>
+                <p><a style="font-size: 13px;">rastorinte@rastorinte.fi</a></p>
+            </div>
+        </div>
+    </div>
+    <p class="copyright">©Copyright reserved to Joonas, Jan, Jami, Otto</p>
+</footer>
 </body>
 </html>

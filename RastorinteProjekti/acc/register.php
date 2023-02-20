953b9505800a6447bd9ac67bb3462279996033f8
janhatanpaa <jan.hatanpaa2@gmail.com>
2023-02-20 12:23:00 +0200

    nav bar acc

--- RastorinteProjekti/acc/register.php
+++ RastorinteProjekti/acc/register.php
@@ -14,9 +14,9 @@
 <nav>
     <div class="navbar grey topBotomBordersIn">
         <a href="../../menu.html" style="font-size: 15px;">Menu</a>
-        <a href="../../index.html" style="font-size: 24px;">Home</a>
+        <a href="../../index.html" style="font-size: 15px;">Home</a>
         <a href="../../gallery.html" style="font-size: 15px;">Gallery</a>
-        <a href="index.php" style="font-size: 15px; position: absolute; top: 27.5%; left: 75%;">Account</a>
+        <a href="index.php" style="font-size: 15px;">Account</a>
     </div>
 </nav>
 <br>
